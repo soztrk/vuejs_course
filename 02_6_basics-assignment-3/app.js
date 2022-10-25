08146b533176ffab418ae5e5f75fac8a75663627
S.Öztürk <s.ozturk999@gmail.com>
2022-10-25 15:07:07 +0300

    Assignment 3 : bug fixed

---
+++ 02_6_basics-assignment-3/app.js
@@ -0,0 +1,38 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            target:37,
+            counter:0,
+            canReset:true
+        }
+    },
+    methods: {
+        add(value){
+            this.counter += value
+        }
+    },
+    computed:{
+        result(){
+            if(this.counter > this.target) return "Too much!"
+            else if(this.counter < this.target) return "Not there yet."
+            else return "You did find the number = "+this.target
+        }
+    },
+    watch:{
+        result(value){
+            console.log(value)
+            const self = this
+            if(self.canReset){
+                setTimeout(()=>{
+                    self.counter = 0
+                    self.canReset = false
+                },5000)
+            }
+            else{
+                self.canReset = true
+            }
+        }
+    }
+})
+
+app.mount("#assignment")
