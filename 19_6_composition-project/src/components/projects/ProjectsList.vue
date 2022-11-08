bb8e032da7c8771c18add2a8615f58b7d109f417
S.Öztürk <s.ozturk999@gmail.com>
2022-11-08 15:45:28 +0300

    project migrate to composition api

---
+++ 19_6_composition-project/src/components/projects/ProjectsList.vue
@@ -0,0 +1,117 @@
+<template>
+  <base-container v-if="props.user">
+    <h2>{{ props.user.fullName }}: Projects</h2>
+    <base-search v-if="hasProjects" @search="updateSearch" :search-term="enteredSearchTerm"></base-search>
+    <ul v-if="hasProjects">
+      <project-item v-for="prj in availableProjects" :key="prj.id" :title="prj.title"></project-item>
+    </ul>
+    <h3 v-else>No projects found.</h3>
+  </base-container>
+  <base-container v-else>
+    <h3>No user selected.</h3>
+  </base-container>
+</template>
+<!--
+<script>
+import ProjectItem from './ProjectItem.vue';
+
+export default {
+  components: {
+    ProjectItem,
+  },
+  props: ['user'],
+  data() {
+    return {
+      enteredSearchTerm: '',
+      activeSearchTerm: '',
+    };
+  },
+  computed: {
+    hasProjects() {
+      return this.user.projects && this.availableProjects.length > 0;
+    },
+    availableProjects() {
+      if (this.activeSearchTerm) {
+        return this.user.projects.filter((prj) =>
+          prj.title.includes(this.activeSearchTerm)
+        );
+      }
+      return this.user.projects;
+    },
+  },
+  methods: {
+    updateSearch(val) {
+      this.enteredSearchTerm = val;
+    },
+  },
+  watch: {
+    enteredSearchTerm(val) {
+      setTimeout(() => {
+        if (val === this.enteredSearchTerm) {
+          this.activeSearchTerm = val;
+        }
+      }, 300);
+    },
+    user() {
+      this.enteredSearchTerm = '';
+    },
+  },
+};
+</script>
+-->
+
+<script setup>
+import{defineProps,ref,computed,watch,toRefs} from "vue"
+import ProjectItem from './ProjectItem.vue'
+
+//Props
+const props = defineProps(["user"])
+
+//Data
+const enteredSearchTerm = ref("")
+const activeSearchTerm = ref("")
+
+//Computed
+const availableProjects = computed(() => {
+  if (activeSearchTerm.value) {
+    return props.user.projects.filter((prj) =>
+      prj.title.includes(activeSearchTerm.value)
+    );
+  }
+  return props.user.projects;
+})
+
+const hasProjects = computed(() => {
+  return props.user.projects && availableProjects.value.length > 0
+})
+
+//Methods
+const updateSearch = (val) => {
+  enteredSearchTerm.value = val;
+}
+
+//Watch
+watch(enteredSearchTerm,(val)=>{
+  setTimeout(() => {
+    if (val === enteredSearchTerm.value) {
+      activeSearchTerm.value = val;
+    }
+  }, 300)
+})
+
+// Props To Refs
+const {user} = toRefs(props)
+
+watch(user,()=>{
+  enteredSearchTerm.value = ""
+})
+
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
